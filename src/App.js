c0aba18fd67608bce1a874188494634cb6f2f157
Dan Burton <danburton720@hotmail.com>
2020-06-06 14:33:26 +0100

    developed sign in component page and added to header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -14,6 +15,7 @@ return (
         <Switch>
             <Route exact path={['/', '/crwn-clothing']} component={HomePage} />
             <Route path={['/shop', '/crwn-clothing/shop']} component={ShopPage} />
+            <Route path={['/signin','/crwn-clothing/signin']} component={SignInAndSignUpPage} />
         </Switch>
     </div>
   )
