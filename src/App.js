44f2118a388e0e0f6a4b61edeec4055aa5db2978
Dan Burton <danburton720@hotmail.com>
2020-06-06 11:09:01 +0100

    adding header component and links to home, shop page, and contact

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
 return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/crwn-clothing' component={HomePage} />
             <Route path='/crwn-clothing/shop' component={ShopPage} />
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import {ReactComponent as Logo } from '../../assets/crown.svg';
+
+import './header.component.styles.scss';
+
+
+const Header = () => (
+  <div className='header'>
+    <Link className='logo-container' to="/crwn-clothing">
+        <Logo className='logo' />
+    </Link>
+      <div className='options'>
+          <Link className='option' to='/crwn-clothing/shop'>
+            SHOP
+          </Link>
+          <Link className='option' to='/crwn-clothing/contact'>
+              CONTACT
+          </Link>
+      </div>
+  </div>
+)
+
+export default Header;
+
