2ab8a69f7d245157f96987019da2d4e119de49d3
Dan Burton <danburton720@hotmail.com>
2020-06-13 14:59:59 +0100

    more fiddling for heroku

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,39 +9,41 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 
 import  './checkout.styles.scss';
 
-const CheckoutPage = ({cartItems, total}) => (
-    <div className='checkout-page'>
-        <div className='checkout-header'>
-            <div className='header-block'>
-                <span>Product</span>
+const CheckoutPage = ({ cartItems, total }) => {
+    return (
+        <div className='checkout-page'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
-            <div className='header-block'>
-                <span>Description</span>
+            {cartItems.map(cartItem => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+            ))}
+            <div className='total'>
+                TOTAL: £{total}
             </div>
-            <div className='header-block'>
-                <span>Quantity</span>
+            <div className='test-warning'>
+                *Please use the following test credit card for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: any date in the future - CVV: 123
             </div>
-            <div className='header-block'>
-                <span>Price</span>
-            </div>
-            <div className='header-block'>
-                <span>Remove</span>
-            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
-        {cartItems.map(cartItem => (
-            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-        <div className='total'>
-            TOTAL: £{total}
-        </div>
-        <div className='test-warning'>
-            *Please use the following test credit card for payments*
-            <br />
-            4242 4242 4242 4242 - Exp: any date in the future - CVV: 123
-        </div>
-        <StripeCheckoutButton price={total} />
-    </div>
-);
+    );
+};
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
