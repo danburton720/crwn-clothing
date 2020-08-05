3e580881cfc573548e3346fac8b6ab739d26286f
Dan Burton <danburton720@hotmail.com>
2020-08-05 18:13:35 +0100

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our WithSpinner HOC

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -12,7 +11,9 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import ContactPage from './pages/contact/contact.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,9 +13,10 @@ const config = {
     measurementId: "G-TMX0RBLM3M"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
-
     const userRef = firestore.doc(`users/${userAuth.uid}`);
 
     const snapShot = await userRef.get();
@@ -39,7 +40,35 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
     return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+       const { title, items } = doc.data();
+
+       return {
+           routeName: encodeURI(title.toLowerCase()),
+           id: doc.id,
+           title,
+           items
+       };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
