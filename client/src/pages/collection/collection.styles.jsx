19454602ca944e866c3c2c2d1e4155a8b0dde2d6
Dan Burton <danburton720@hotmail.com>
2020-08-20 19:18:16 +0100

    Added media queries to make the app responsive

--- client/src/pages/collection/collection.styles.jsx
+++ client/src/pages/collection/collection.styles.jsx
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 export const CollectionPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  @media screen and (max-width: 800px) {
+    align-items: center;  
+  }
 `;
 
 export const CollectionTitle = styled.h2`
   font-size: 38px;
   margin: 0 auto 30px;
+  @media screen and (max-width: 800px) {
+    font-size: 28px;
+  }
 `;
 
 export const CollectionItemsContainer = styled.div`
@@ -16,5 +22,11 @@ export const CollectionItemsContainer = styled.div`
   grid-gap: 10px;
   & > div {
     margin-bottom: 30px;
+  }
+  
+  @media screen and (max-width: 800px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 15px;
   }
 `;
